82901329d5b9120a3c9b0f7de662af9263759985
RENAN029 <pires.s@escolar.ifrn.edu.br>
2026-01-04 13:00:47 -0300

    Update package installation in c.sh

--- c.sh
+++ c.sh
@@ -2,7 +2,8 @@ set -e
 
 sudo pacman -S --noconfirm noto-fonts noto-fonts-cjk noto-fonts-emoji ttf-noto-nerd noto-fonts-extra ttf-jetbrains-mono
 sudo pacman -S --noconfirm ffmpeg gst-plugins-ugly gst-plugins-good gst-plugins-base gst-plugins-bad gst-libav gstreamer
-sudo pacman -S --noconfirm cosmic-session cosmic-terminal cosmic-files cosmic-store cosmic-wallpapers xdg-user-dirs gdu 
+sudo pacman -S --noconfirm cosmic-session cosmic-terminal cosmic-files cosmic-store cosmic-wallpapers xdg-user-dirs croc
+sudo pacman -S --noconfirm gdu
 
 nvidia-open intel-ucode git neovim fastfetch btop ufw fwupd flatpak yt-dlp aria2 tealdeer
  
